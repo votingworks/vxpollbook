e95e80f4170ebc3e2c61eccd5865b6de191900c9
carolinemodic <caroline.modic+github@gmail.com>
2025-01-09 15:45:46 -0800

    update configuration scripts for networking (#37)

--- script/setup_basic_mesh.sh
+++ script/setup_basic_mesh.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-CURRENT_IP=${CURRENT_IP:-192.168.1.1}
 # This script creates a basic mesh network assuming that there is a external wireless adapter plugged in with the mode "mesh point" supported.
 # Note this will only work after disabling NetworkManager which conflicts with the setup here. That can be disabled with:
 # sudo systemctl stop NetworkManager
 
+# Check if mesh0 interface already exists, if so reconnect to it instead of creating from scratch.
+if iw dev | grep -q "mesh0"; then
+    echo "mesh0 interface already exists. Bringing it up and joining pollbook_mesh."
+    sleep 1
+    sudo iw dev mesh0 mesh join pollbook_mesh
+    echo "Successfully joined the network."
+    exit 0
+fi
+
 wireless_interface=$(iw dev | awk '/Interface/ {print $2}' | grep -v "wlp9s0")
 if [ -z "$wireless_interface" ]; then
-	echo "No wireless interface found."
-	exit 1
+    echo "No wireless interface found."
+    exit 1
 fi
 
 echo "Found wireless interface: $wireless_interface"
@@ -22,14 +30,15 @@ sleep 0.1 # the interface gets renamed by udev rules wait for that to happen so 
 new_interface=$(iw dev | awk '/Interface/ {print $2}' | grep -v "wlp9s0" | grep -v "$wireless_interface")
 echo "New interface name: $new_interface"
 if [ -n "$new_interface" ] && [ "$new_interface" != "mesh0" ]; then
-	echo "Renaming interface from: $new_interface to mesh0"
-	sudo ip link set "$new_interface" name mesh0
+    echo "Renaming interface from: $new_interface to mesh0"
+    sudo ip link set "$new_interface" name mesh0
 fi
 
 echo "Bringing up the network and joining pollbook_mesh"
 sudo ip link set mesh0 up
 sudo iw dev mesh0 mesh join pollbook_mesh
 
-sudo ip addr add $CURRENT_IP/24 dev mesh0
+# Use avahi-autoipd to get a link-local IP address
+sudo avahi-autoipd -D -w mesh0
 
-echo "Successfully joined the network." 
+echo "Successfully joined the network."

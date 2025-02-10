d1ef36d9666ad7c3d11dafafd19423304811b897
Caroline Mars <caroline@voting.works>
2025-02-10 09:09:32 -0800

    always show reset button, timeout after 10s

--- frontend/src/nav_screen.tsx
+++ frontend/src/nav_screen.tsx
@@ -169,11 +169,9 @@ function NetworkStatus({ status }: { status: NetworkStatus }) {
           }
           actions={
             <React.Fragment>
-              {status.isOnline ? null : (
-                <Button onPress={resetNetworkConnection} color="primary">
-                  Reset Network
-                </Button>
-              )}
+              <Button onPress={resetNetworkConnection} color="primary">
+                Reset Network
+              </Button>
               <Button onPress={() => setShowModal(false)}>Close</Button>
             </React.Fragment>
           }

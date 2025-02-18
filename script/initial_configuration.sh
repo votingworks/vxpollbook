63556b1f7496c7c9f030446a5839408cee6858ed
Caroline Mars <caroline@voting.works>
2025-02-18 11:12:10 -0800

    ipsec test config

--- script/initial_configuration.sh
+++ script/initial_configuration.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+sudo apt install avahi-daemon avahi-utils avahi-autoipd strongswan -y
+
+sudo cp mesh-ipsec.conf /etc/ipsec.conf
+sudo cp avahi-autoipd.action /etc/avahi/avahi-autoipd.action
 sudo cp run-vxpollbook.sh /vx/scripts/.
 sudo cp update-vxpollbook.sh /vx/scripts/.
+sudo cp update-ipsec.sh /vx/scripts/.
 sudo cp run-vxpollbook.desktop /usr/share/applications/.
 sudo cp update-vxpollbook.desktop /usr/share/applications/.
 gsettings set org.gnome.shell favorite-apps "['run-vxpollbook.desktop', 'update-vxpollbook.desktop', 'org.gnome.Screenshot.desktop', 'firefox-esr.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
 
-sudo apt install avahi-daemon avahi-utils avahi-autoipd
 sudo systemctl disable NetworkManager
 sudo systemctl disable firewalld
 sudo systemctl stop NetworkManager

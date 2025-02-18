fe4e1224c3463d2e9ad9540b277d470e9dd1b7e8
Caroline Mars <caroline@voting.works>
2025-02-18 11:39:23 -0800

    fix action file

--- script/update-ipsec.sh
+++ script/update-ipsec.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Pull the current mesh0 IP (assuming it's IPv4)
-MESH_IP=$(ip -4 addr show mesh0 | awk '/inet / {print $2}' | cut -d/ -f1)
-
-if [[ -z "$MESH_IP" ]]; then
-    echo "No IPv4 address found on mesh0; cannot update IPsec."
+if [ -z "$1" ]; then
+    echo "No IP address provided; cannot update IPsec."
     exit 1
 fi
 
-echo "Updating IPsec config with mesh0 IP: $MESH_IP"
+MESH_IP="$1"
+echo "Updating IPsec config with new IP: $MESH_IP"
 
 # Update /etc/ipsec.conf:
 #   - Replaces the line starting with "left=" in the "meshvpn" connection
